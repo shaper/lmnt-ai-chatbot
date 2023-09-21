13399864e40841366462b3cca102649e54f3e849
Walter Korman <shaper@lmnt.com>
2023-09-20 17:24:05 -0700

    Add LMNT to footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,9 +13,12 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       {...props}
     >
       Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
+      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>,{' '}
       <ExternalLink href="https://vercel.com/storage/kv">
         Vercel KV
+      </ExternalLink> and{' '}
+      <ExternalLink href="https://lmnt.com">
+        LMNT
       </ExternalLink>
       .
     </p>
